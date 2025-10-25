ff4ce89140e2448c00f377331048798770397d34
pedrosakaue <pedro.sakaue@sptech.school>
2025-10-25 15:30:47 -0300

    refactor: ajustando classes database

--- utils/Database.py
+++ utils/Database.py
@@ -1,3 +1,5 @@
+# utils/Database.py
+
 from mysql.connector import connect, Error
 from dotenv import load_dotenv
 import os
@@ -6,14 +8,16 @@ load_dotenv()
 
 def Fazer_consulta_banco(config):
     """
-    Recebe um dicionário com:
-        config = {
-            "query": "SQL AQUI",
-            "params": (param1, param2, ...)
-        }
+    Função de utilidade que centraliza a execução de consultas SQL.
+    Lança RuntimeError em caso de falha no DB.
+
+    Retorna:
+    - Para SELECT: Lista de tuplas (resultado da consulta).
+    - Para INSERT: O ID da linha inserida (cursor.lastrowid).
+    - Para UPDATE/DELETE: O número de linhas afetadas (cursor.rowcount).
     """
     instrucaoSQL = config.get("query")
-    valores = config.get("params", None)  # None caso não tenha parâmetros
+    valores = config.get("params", None)
 
     db_config = {
         'user': os.getenv("USER_DB"),
@@ -27,21 +31,29 @@ def Fazer_consulta_banco(config):
         cursor = conn.cursor()
 
         if valores:
-            # garante que valores é uma tupla
             cursor.execute(instrucaoSQL, valores if isinstance(valores, tuple) else (valores,))
         else:
             cursor.execute(instrucaoSQL)
 
-        if instrucaoSQL.strip().lower().startswith("select"):
+        sql_tipo = instrucaoSQL.strip().lower()
+
+        if sql_tipo.startswith("select"):
             resultado = cursor.fetchall()
             conn.close()
             return resultado
         else:
             conn.commit()
-            linhas = cursor.rowcount
-            conn.close()
-            return linhas
+            
+            if sql_tipo.startswith("insert"):
+                id_inserido = cursor.lastrowid
+                conn.close()
+                return id_inserido
+            else:
+                linhas = cursor.rowcount
+                conn.close()
+                return linhas
 
     except Exception as e:
-        print("Error to connect with MySQL -", e)
-        return None
+        error_message = f"Error to connect with MySQL - {e}"
+        print(error_message)
+        raise RuntimeError(error_message)
